9674e15aff0741252d95b356366a8f9254e00ce7
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-05-22 22:24:19 +0200

    .env astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
+import dotenv from 'dotenv';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
+dotenv.config();
+
 export default defineConfig({
   integrations: [tailwind()]
 });
