8e087c9e42f033412ab38de9d52c123c8e51121d
Tonips22 <130395782+Tonips22@users.noreply.github.com>
2024-05-23 10:42:01 +0200

    Remove dotnev useless deoendency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import dotenv from 'dotenv';
 import tailwind from "@astrojs/tailwind";
 
-dotenv.config();
 
 export default defineConfig({
   integrations: [tailwind()]
